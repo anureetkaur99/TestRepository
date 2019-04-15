6b2a7087947609f02c32b9fddfc71d49db76317a
Ong Thao <hellopanmoo@gmail.com>
2019-04-15 10:07:17 -0700

    testing

--- Test2/src/Test2.java
+++ Test2/src/Test2.java
@@ -3,5 +3,9 @@ public class Test2 {
 	public static void main (String[] args) {
 		System.out.println("Say Bye!");
 		System.out.print("Life sucks!!");
+		System.out.print(18 + "years");
+		int n = 18;
+		String s = "yellow";
+		System.out.print(n + s);
 	}
 }
